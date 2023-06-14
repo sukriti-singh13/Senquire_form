dcb000858ecbbdeac1f6376e27b58983a4fb546b
sukriti singh <sukriti.singh899@gmail.com>
2023-06-15 04:43:50 +0530

    initialised project

---
+++ src/components/Main_config.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main_config = () => {
+  return (
+    <div>Main_config</div>
+  )
+}
+
+export default Main_config
---
+++ src/components/Sub_config.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sub_config = () => {
+  return (
+    <div>Sub_config</div>
+  )
+}
+
+export default Sub_config
